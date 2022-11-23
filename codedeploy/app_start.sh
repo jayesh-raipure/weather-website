3c77627c06db537e3ac6dcb67ac8a6a311ede06a
Jayesh Raipure <jayesh.raipure@excellarate.com>
2022-11-23 15:46:19 +0530

    Added validate_service.sh

--- codedeploy/app_start.sh
+++ codedeploy/app_start.sh
@@ -1,4 +1,4 @@
-sudo chown ubuntu:ubuntu /home/ubuntu/weather-website
+sudo chown -R ubuntu:ubuntu /home/ubuntu/weather-website
 cd /home/ubuntu/weather-website
 source /home/ubuntu/.bashrc
 npm install
